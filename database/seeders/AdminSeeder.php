129841dac24078f1e19d23a691552c1e14c34eae
mdnaseeransari <mdnaseeransari0@gmail.com>
2026-05-11 22:20:37 +0530

    changed docker command

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -8,9 +8,7 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    
     public function run(): void
     {
         User::create([
